8f2e49a4fff7f49c160495b078ae08a1c886f453
Paul Razvan Berg <paul.razvan.berg@gmail.com>
2023-09-20 17:49:20 +0300

    First draft

--- README.md
+++ README.md
@@ -1,9 +1,43 @@
-# Sablier V2 Integration Template
+# Sablier V2 🤝 SteakHut
 
-This repository contains a template for building integrations with Sablier. For more information, refer to this guide on
-our documentation website:
+This repository contains a Forge script developed by the Sablier Labs team for SteakHut. The script batch creates
+multiple Sablier streams, which will power SteakHut's
+[$STEAK](https://snowtrace.io/token/0xb279f8DD152B99Ec1D84A489D32c35bC0C7F5674) token vesting campaign.
 
-https://docs.sablier.com/contracts/v2/guides/local-environment
+## Usage
+
+Run the script like this, making sure to replace `ETH_FROM` with your wallet address:
+
+```sh
+# your address here
+ETH_FROM=0x0000000000000000000000000000000000000000 \
+forge script script/SteakHut.s.sol \
+--broadcast \
+--ledger \
+--mnemonic-derivation-paths  "m/44'/60'/0'/0" \
+--rpc-url avalanche \
+--sig "run()" \
+--verify \
+--with-gas-price $(cast to-wei 25 gwei) \
+-vvvv
+```
+
+## Notes
+
+- Run `foundryup` to make sure you are running the latest version of Foundry
+- The command above assumes that you are using a Ledger wallet connected to a computer via USB. If you wish to use a
+  mnemonic instead, you can set it as a `$MNEMONIC` environment variable in a `.env` file. Check out the
+  [`Base`](https://github.com/sablier-labs/v2-core/blob/d1157b49ed4bceeff0c4e437c9f723e88c134d3a/test/Base.t.sol)
+  script.
+
+## References
+
+- [Sablier Docs](https://docs.sablier.com)
+
+## Caveat Emptor
+
+This is experimental software and is provided on an "as is" and "as available" basis. Sablier Labs does not give any
+warranties and will not be liable for any loss, direct or indirect through continued use of this codebase.
 
 ## License
 
---
+++ script/SteakHut.s.sol
@@ -0,0 +1,168 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity >=0.8.19;
+
+import { ISablierV2LockupDynamic } from "@sablier/v2-core/src/interfaces/ISablierV2LockupDynamic.sol";
+import { LockupDynamic } from "@sablier/v2-core/src/types/DataTypes.sol";
+import { ud2x18 } from "@sablier/v2-core/src/types/Math.sol";
+import { IERC20 } from "@sablier/v2-core/src/types/Tokens.sol";
+import { BaseScript } from "@sablier/v2-core-script/Base.s.sol";
+import { ISablierV2ProxyTarget } from "@sablier/v2-periphery/src/interfaces/ISablierV2ProxyTarget.sol";
+import { Batch } from "@sablier/v2-periphery/src/types/DataTypes.sol";
+import { IPRBProxy, IPRBProxyPlugin, IPRBProxyRegistry } from "@sablier/v2-periphery/src/types/Proxy.sol";
+
+contract SteakHutScript is BaseScript {
+    /*//////////////////////////////////////////////////////////////////////////
+                                    STEAK PARAMS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    uint128 public constant AMOUNT_0 = 320_901.64e18;
+    uint128 public constant AMOUNT_1 = 122_950.82e18;
+    uint128 public constant AMOUNT_2 = 81_967.21e18;
+    uint128 public constant AMOUNT_3 = 32_786.89e18;
+    uint128 public constant AMOUNT_4 = 8196.72e18;
+    uint128 public constant AMOUNT_5 = 8196.72e18;
+    IERC20 public constant STEAK = IERC20(0xb279f8DD152B99Ec1D84A489D32c35bC0C7F5674);
+    uint128 public constant TOTAL_TRANSFER_AMOUNT = 575_000e18;
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                   SABLIER PARAMS
+    //////////////////////////////////////////////////////////////////////////*/
+
+    // Check the address: https://prbproxy.com/
+    IPRBProxyRegistry public constant PROXY_REGISTRY = IPRBProxyRegistry(0x584009E9eDe26e212182c9745F5c000191296a78);
+
+    // Check the addresses in the docs: https://docs.sablier.com/contracts/v2/deployments
+    ISablierV2LockupDynamic public constant SABLIER_LOCKUP_DYNAMIC =
+        ISablierV2LockupDynamic(0x665d1C8337F1035cfBe13DD94bB669110b975f5F);
+    address public constant SABLIER_PROXY_PLUGIN = 0x17167A7e2763121e263B4331B700a1BF9113b387;
+    ISablierV2ProxyTarget public immutable SABLIER_PROXY_TARGET =
+        ISablierV2ProxyTarget(0xD87d75ceE7b7c5B9FAC5F9b37C55B53F682B9058);
+
+    /*//////////////////////////////////////////////////////////////////////////
+                                    SCRIPT LOGIC
+    //////////////////////////////////////////////////////////////////////////*/
+
+    function run() public virtual broadcast returns (uint256[] memory streamIds) {
+        // Deploy an instance of PRBProxy. For more info, see:
+        // https://docs.sablier.com/contracts/v2/guides/proxy-architecture/overview
+        IPRBProxy proxy = PROXY_REGISTRY.getProxy({ user: address(this) });
+        if (address(proxy) == address(0)) {
+            proxy = PROXY_REGISTRY.deployAndInstallPlugin({ plugin: IPRBProxyPlugin(SABLIER_PROXY_PLUGIN) });
+        }
+
+        // Approve the proxy to transfer $STEAK
+        uint256 allowance = STEAK.allowance({ owner: address(this), spender: address(proxy) });
+        if (allowance < TOTAL_TRANSFER_AMOUNT) {
+            STEAK.approve({ spender: address(proxy), amount: TOTAL_TRANSFER_AMOUNT });
+        }
+
+        // Get the batch data
+        Batch.CreateWithDeltas[] memory batch = getBatch(address(proxy));
+
+        // Encode the data for the proxy
+        bytes memory data = abi.encodeCall(
+            SABLIER_PROXY_TARGET.batchCreateWithDeltas, (SABLIER_LOCKUP_DYNAMIC, STEAK, batch, bytes(""))
+        );
+
+        // Create a batch of streams via the proxy and Sablier's proxy target
+        bytes memory response = proxy.execute(address(SABLIER_PROXY_TARGET), data);
+        streamIds = abi.decode(response, (uint256[]));
+    }
+
+    function getSegmentsForUser(uint256 userIndex) public pure returns (LockupDynamic.SegmentWithDelta[] memory) {
+        uint128 firstAmount;
+        uint128 secondAmount;
+        uint128 thirdAmount;
+
+        if (userIndex == 0) {
+            firstAmount = 16_045.082e18; // 5% of 320,901.64
+            secondAmount = 101_597.459224e18; // 31.66% of 320,901.64
+            thirdAmount = AMOUNT_0 - firstAmount - secondAmount; // remainder
+        } else if (userIndex == 1) {
+            firstAmount = 6147.541e18; // 5% of 122,950.82
+            secondAmount = 38_926.229612e18; // 31.66% of 122,950.82
+            thirdAmount = AMOUNT_1 - firstAmount - secondAmount; // remainder
+        } else if (userIndex == 2) {
+            firstAmount = 4098.3605e18; // 5% of 81,967.21
+            secondAmount = 25_950.818686e18; // 31.66% of 81,967.21
+            thirdAmount = AMOUNT_2 - firstAmount - secondAmount; // remainder
+        } else if (userIndex == 3) {
+            firstAmount = 1639.3445e18; // 5% of 32,786.89
+            secondAmount = 10_380.329374e18; // 31.66% of 32,786.89
+            thirdAmount = AMOUNT_3 - firstAmount - secondAmount; // remainder
+        } else if (userIndex == 4) {
+            firstAmount = 409.836e18; // 5% of 8196.72
+            secondAmount = 2595.081552e18; // 31% of 8196.72
+            thirdAmount = AMOUNT_4 - firstAmount - secondAmount; // remainder
+        } else if (userIndex == 5) {
+            firstAmount = 409.836e18; // 5% of 8196.72
+            secondAmount = 2595.081552e18; // 31% of 8196.72
+            thirdAmount = AMOUNT_5 - firstAmount - secondAmount; // remainder
+        } else {
+            revert("Invalid user index");
+        }
+
+        // The following four segments will produce the following effect:
+        //
+        //   - 5% unlock after 1 day
+        //   - 31.66% unlock after 1 year cliff
+        //   - 63.34% linear streaming for 6 months
+        //
+        // For more guidance, see https://docs.sablier.com/concepts/protocol/stream-types#lockup-dynamic
+        LockupDynamic.SegmentWithDelta[] memory segments = new LockupDynamic.SegmentWithDelta[](4);
+        segments[0] = LockupDynamic.SegmentWithDelta({ amount: firstAmount, delta: 1 days, exponent: ud2x18(1) });
+        segments[1] = (LockupDynamic.SegmentWithDelta({ amount: 0, delta: 31_449_599 seconds, exponent: ud2x18(1) }));
+        segments[2] =
+            (LockupDynamic.SegmentWithDelta({ amount: secondAmount, delta: 31_449_600 seconds, exponent: ud2x18(1) }));
+        segments[3] = (
+            LockupDynamic.SegmentWithDelta({
+                amount: thirdAmount,
+                delta: 15_778_476 seconds, // ~6 months
+                exponent: ud2x18(1)
+            })
+        );
+        return segments;
+    }
+
+    function getBatch(address proxy) public pure returns (Batch.CreateWithDeltas[] memory) {
+        Batch.CreateWithDeltas[] memory batch = new Batch.CreateWithDeltas[](6);
+
+        batch[0].sender = address(proxy);
+        batch[0].recipient = address(0xa912A42A40BE74A0ab8a1C9a4f970590AC733Fe7);
+        batch[0].totalAmount = AMOUNT_0;
+        batch[0].cancelable = false;
+        batch[0].segments = getSegmentsForUser({ userIndex: 0 });
+
+        batch[1].sender = address(proxy);
+        batch[1].recipient = address(0xd24fa760E600081370c2730365B646A0bE46FCd0);
+        batch[1].totalAmount = AMOUNT_1;
+        batch[1].cancelable = false;
+        batch[1].segments = getSegmentsForUser({ userIndex: 1 });
+
+        batch[2].sender = address(proxy);
+        batch[2].recipient = address(0xfBADAAb2950349a6AD82B2Eb9060E0cd008d4d2C);
+        batch[2].totalAmount = AMOUNT_2;
+        batch[2].cancelable = false;
+        batch[2].segments = getSegmentsForUser({ userIndex: 2 });
+
+        batch[3].sender = address(proxy);
+        batch[3].recipient = address(0x17ECe27ffb9053C797e1Aea6eebc4F9A9BcD6828);
+        batch[3].totalAmount = AMOUNT_3;
+        batch[3].cancelable = false;
+        batch[3].segments = getSegmentsForUser({ userIndex: 3 });
+
+        batch[4].sender = address(proxy);
+        batch[4].recipient = address(0xED6bbDA3b9bDcF9Da51D24493c3Cd4F7141A6b7e);
+        batch[4].totalAmount = AMOUNT_4;
+        batch[4].cancelable = false;
+        batch[4].segments = getSegmentsForUser({ userIndex: 4 });
+
+        batch[5].sender = address(proxy);
+        batch[5].recipient = address(0x11519Af72D6a9f9eE339afD0Aa9925d199a8cc79);
+        batch[5].totalAmount = AMOUNT_5;
+        batch[5].cancelable = false;
+        batch[5].segments = getSegmentsForUser({ userIndex: 5 });
+
+        return batch;
+    }
+}
